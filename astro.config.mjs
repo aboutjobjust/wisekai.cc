93a11838cff30d0c8a38bb7c66032288bd594d8d
aboutjobjust <aboutjobjsut3333@gmail.com>
2024-05-03 17:55:25 +0900

    npx astro add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import purgecss from "astro-purgecss";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), purgecss()]
+  integrations: [tailwind(), svelte(), purgecss(), mdx()]
 });
