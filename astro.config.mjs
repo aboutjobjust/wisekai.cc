45261979229e3b863c845734940b17f660374477
aboutjobjust <aboutjobjsut3333@gmail.com>
2024-05-03 17:51:37 +0900

    npx astro add astro-purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte(), purgecss()]
 });
