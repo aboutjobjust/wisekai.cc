758e083d6ececb9eac02b7f571f51831721fab7d
aboutjobjust <aboutjobjsut3333@gmail.com>
2024-09-08 18:20:12 +0900

    Twitter埋め込みの調整

--- src/components/JTimeLine.tsx
+++ src/components/JTimeLine.tsx
@@ -11,15 +11,16 @@ export const JTimeLine: Component = () => {
   });
 
   return (
-    <div class="max-w-full flex-shrink-0">
+    <div class="h-[600px] w-[500px] max-w-full flex-shrink-0">
       <a
-        class="twitter-timeline block h-[600px] w-[500px]"
+        class="twitter-timeline relative -z-10 block h-full w-full rounded-md border border-gray-300"
         data-lang="ja"
         data-width="500"
         data-height="600"
-        aria-label="Tweets by isekaijoucho"
         href="https://twitter.com/isekaijoucho?ref_src=twsrc%5Etfw"
-      ></a>
+      >
+        <span class="loader">Tweets by isekaijoucho</span>
+      </a>
     </div>
   );
 };
