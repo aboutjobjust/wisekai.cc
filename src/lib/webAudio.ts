a4a137db4a9e052d5253259235172d7028628825
aboutjobjust <aboutjobjsut3333@gmail.com>
2024-09-16 14:56:38 +0900

    chromeでも動作

--- src/lib/webAudio.ts
+++ src/lib/webAudio.ts
@@ -1,10 +1,11 @@
 export class Wa {
-  private src: string;
   private context: AudioContext;
   private buffer: AudioBuffer;
   private source: AudioBufferSourceNode | undefined;
+  private audioElement: HTMLAudioElement | undefined;
   private _isPlaying = false;
   private changePlayingListener: () => void = () => {};
+  private isIOS = /iP(hone|(o|a)d)/.test(navigator.userAgent);
 
   public static async init(src: string) {
     const context = new (window.AudioContext ||
@@ -14,8 +15,14 @@ export class Wa {
         context.resume();
       }
     };
-    const buffer = await this.loadFile(src, context);
-    return new this(src, context, buffer);
+
+    if (/iP(hone|(o|a)d)/.test(navigator.userAgent)) {
+      const buffer = await this.loadFile(src, context);
+      return new this(context, buffer, undefined);
+    } else {
+      const audioElement = new Audio(src);
+      return new this(context, undefined, audioElement);
+    }
   }
 
   private static async loadFile(
@@ -32,33 +39,54 @@ export class Wa {
   }
 
   private constructor(
-    _src: string,
     _context: AudioContext,
-    _buffer: AudioBuffer,
+    _buffer: AudioBuffer | undefined,
+    _audioElement: HTMLAudioElement | undefined,
   ) {
-    this.src = _src;
     this.context = _context;
-    this.buffer = _buffer;
+    this.buffer = _buffer!;
+    this.audioElement = _audioElement!;
   }
 
   public async play(time?: number): Promise<void> {
     if (this.isPlaying) {
       this.stop();
     }
-    await this.context.resume();
-    this.source = this.context.createBufferSource();
-    this.source.buffer = this.buffer;
-    this.source.connect(this.context.destination);
-    this.source.start(time);
-    this.isPlaying = true;
-    this.source.onended = () => {
-      this.isPlaying = false;
-    };
+
+    if (this.isIOS) {
+      await this.context.resume();
+      this.source = this.context.createBufferSource();
+      this.source.buffer = this.buffer;
+      this.source.connect(this.context.destination);
+      this.source.start(time);
+      this.isPlaying = true;
+      this.source.onended = () => {
+        this.isPlaying = false;
+      };
+    } else if (this.audioElement) {
+      this.audioElement.currentTime = time || 0;
+      await new Promise<void>((resolve) => {
+        setTimeout(() => {
+          this.audioElement?.play();
+          resolve();
+        }, 50);
+      });
+      this.isPlaying = true;
+      this.audioElement.onended = () => {
+        this.isPlaying = false;
+      };
+    }
   }
 
   public stop(): void {
-    this.source?.stop();
-    this.isPlaying = false;
+    if (this.isIOS && this.source) {
+      this.source.stop();
+      this.isPlaying = false;
+    } else if (this.audioElement) {
+      this.audioElement.pause();
+      this.audioElement.currentTime = 0;
+      this.isPlaying = false;
+    }
   }
 
   public onChangePlaying(_changePlayingListener: () => void): void {
