fae317c6397c14ab3af34a5481b9eab6c09436fc
aboutjobjust <aboutjobjsut3333@gmail.com>
2024-09-16 22:31:03 +0900

    軽量化

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,30 +7,26 @@ import purgecss from 'astro-purgecss';
 import robotsTxt from 'astro-robots-txt';
 import { defineConfig } from 'astro/config';
 
+import playformInline from '@playform/inline';
+
+import playformCompress from '@playform/compress';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wisekai.cc',
   compressHTML: import.meta.env.PROD,
-  integrations: [
-    tailwind(),
-    purgecss({
-      safelist: {
-        greedy: [/^splide/],
+  integrations: [tailwind(), purgecss({
+    safelist: {
+      greedy: [/^splide/],
+    },
+    extractors: [
+      {
+        extractor: (content) =>
+          content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
+        extensions: ['astro', 'tsx', 'html'],
       },
-      extractors: [
-        {
-          extractor: (content) =>
-            content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
-          extensions: ['astro', 'tsx', 'html'],
-        },
-      ],
-    }),
-    mdx(),
-    solidJs(),
-    icon(),
-    robotsTxt(),
-    sitemap(),
-  ],
+    ],
+  }), mdx(), solidJs(), icon(), robotsTxt(), sitemap(), playformInline(), playformCompress()],
   vite: {
     ssr: {
       noExternal: ['@splidejs'],
